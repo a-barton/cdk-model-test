431299501ebdfe76921b095bde3b5a80881d9598
Andrew Barton <72054220+a-barton@users.noreply.github.com>
2022-02-19 13:02:14 +1100

    Merging step-function back into main (#6)
    * Built out step function, lambda and S3 bucket in Sagemaker stack
    
    * Refining submit training step function
    
    * Got training step function working with Sagemaker

--- src/cdk_stacks/sagemaker_stack.py
+++ src/cdk_stacks/sagemaker_stack.py
@@ -1,9 +1,16 @@
+import os
 from aws_cdk import (
     CfnParameter,
     Stack,
     App,
     aws_iam as iam,
     aws_sagemaker as sagemaker,
+    aws_lambda as _lambda,
+    aws_stepfunctions as _aws_stepfunctions,
+    aws_stepfunctions_tasks as _aws_stepfunctions_tasks,
+    aws_s3,
+    aws_s3_deployment,
+    RemovalPolicy,
 )
 from aws_cdk.aws_ecr_assets import DockerImageAsset
 
@@ -14,15 +21,43 @@ class SagemakerStack(Stack):
 
         MODEL_NAME = config["MODEL_NAME"]
 
-        # Define resource name parameters
+        #########################
+        ## Sagemaker resources ##
+        #########################
+
+        # Define Cloudformation parameters
         model_name = CfnParameter(
             self, "model", type="String", default=MODEL_NAME,
         ).value_as_string
 
         # Define Docker image for the model, referencing the local Dockerfile in the repo
-        asset = DockerImageAsset(self, "MLInferenceImage", directory="src/container")
+        docker_image_asset = DockerImageAsset(
+            self, "MLInferenceImage", directory="src/container"
+        )
         primary_container_definition = sagemaker.CfnModel.ContainerDefinitionProperty(
-            image=asset.image_uri,
+            image=docker_image_asset.image_uri,
+        )
+
+        # Define S3 bucket for modelling data/artifacts
+        bucket = aws_s3.Bucket(
+            self,
+            "S3Bucket",
+            bucket_name=f"{model_name}-bucket",
+            removal_policy=RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+        )
+
+        # Add training data to the S3 bucket
+        __dirname = os.path.dirname(os.path.realpath(__file__))
+        training_data_bucket_deployment = aws_s3_deployment.BucketDeployment(
+            self,
+            "TrainingData",
+            sources=[
+                aws_s3_deployment.Source.asset(
+                    os.path.join(__dirname, "../../data/train/")
+                )
+            ],
+            destination_bucket=bucket,
         )
 
         # Execution role that SageMaker will assume to run the model
@@ -30,13 +65,35 @@ class SagemakerStack(Stack):
             self,
             "SageMakerExecutionRole",
             assumed_by=iam.ServicePrincipal("sagemaker.amazonaws.com"),
+            inline_policies=[
+                iam.PolicyDocument(
+                    statements=[
+                        iam.PolicyStatement(
+                            actions=["s3:*"],
+                            resources=[
+                                f"{bucket.bucket_arn}",
+                                f"{bucket.bucket_arn}/*",
+                            ],
+                        ),
+                        iam.PolicyStatement(
+                            actions=[
+                                "ecr:BatchCheckLayerAvailability",
+                                "ecr:BatchGetImage",
+                                "ecr:GetDownloadUrlForLayer",
+                            ],
+                            resources=["*"],
+                        ),
+                    ]
+                ),
+            ],
             managed_policies=[
                 iam.ManagedPolicy.from_aws_managed_policy_name(
-                    "AmazonSageMakerFullAccess"
-                )
+                    "AmazonSageMakerFullAccess",
+                ),
             ],
         )
 
+        # Sagemaker model itself, referencing container definition from above and execution role
         sagemaker_model = sagemaker.CfnModel(
             self,
             "SagemakerModel",
@@ -45,27 +102,91 @@ class SagemakerStack(Stack):
             primary_container=primary_container_definition,
         )
 
-        model_endpoint_config = sagemaker.CfnEndpointConfig(
+        # Endpoint configuration, referencing the model created above
+        # model_endpoint_config = sagemaker.CfnEndpointConfig(
+        #     self,
+        #     "ModelEndpointConfig",
+        #     endpoint_config_name=f'config-{model_name.replace("_","-").replace("/","--")}',
+        #     production_variants=[
+        #         sagemaker.CfnEndpointConfig.ProductionVariantProperty(
+        #             model_name=sagemaker_model.model_name,
+        #             initial_instance_count=1,
+        #             initial_variant_weight=1.0,
+        #             instance_type="ml.t2.medium",
+        #             variant_name="production-medium",
+        #         )
+        #     ],
+        # )
+
+        # Endpoint, referencing the endpoint configuration created above
+        # model_endpoint = sagemaker.CfnEndpoint(
+        #     self,
+        #     "ModelEndpoint",
+        #     endpoint_name=f'endpoint-{model_name.replace("_","-").replace("/","--")}',
+        #     endpoint_config_name=model_endpoint_config.endpoint_config_name,
+        # )
+
+        # model_endpoint_config.node.add_dependency(sagemaker_model)
+        # model_endpoint.node.add_dependency(model_endpoint_config)
+
+        ###########################
+        ## Stepfunction workflow ##
+        ###########################
+
+        training_job_config = {
+            "training_image": docker_image_asset.image_uri,
+            "model_name": MODEL_NAME,
+            "role_arn": sagemaker_execution_role.role_arn,
+            "train_data_uri": bucket.s3_url_for_object(),
+            "resource_config": {
+                "instance_type": "ml.m5.large",
+                "instance_count": 1,
+                "volume_size": 10,
+            },
+            "use_spot_training": True,
+        }
+
+        training_submit_lambda_role = iam.Role(
             self,
-            "ModelEndpointConfig",
-            endpoint_config_name=f'config-{model_name.replace("_","-").replace("/","--")}',
-            production_variants=[
-                sagemaker.CfnEndpointConfig.ProductionVariantProperty(
-                    model_name=sagemaker_model.model_name,
-                    initial_instance_count=1,
-                    initial_variant_weight=1.0,
-                    instance_type="ml.t2.medium",
-                    variant_name="production-medium",
-                )
-            ],
+            "submitTrainingLambdaRole",
+            assumed_by=iam.ServicePrincipal("lambda.amazonaws.com"),
         )
 
-        model_endpoint = sagemaker.CfnEndpoint(
-            self,
-            "ModelEndpoint",
-            endpoint_name=f'endpoint-{model_name.replace("_","-").replace("/","--")}',
-            endpoint_config_name=model_endpoint_config.endpoint_config_name,
+        training_submit_lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=[
+                    "sagemaker:CreateTrainingJob",
+                    "sagemaker:DescribeTrainingJob",
+                ],
+                resources=["*"],
+            ),
+        )
+        training_submit_lambda_role.add_to_policy(
+            iam.PolicyStatement(
+                effect=iam.Effect.ALLOW,
+                actions=["iam:PassRole"],
+                resources=[sagemaker_execution_role.role_arn],
+            ),
         )
 
-        model_endpoint_config.node.add_dependency(sagemaker_model)
-        model_endpoint.node.add_dependency(model_endpoint_config)
+        training_submit_lambda = _lambda.Function(
+            self,
+            "submitTrainingLambda",
+            handler="app.lambda_handler",
+            runtime=_lambda.Runtime.PYTHON_3_9,
+            code=_lambda.Code.from_asset("src/lambdas/submit_training/"),
+            role=training_submit_lambda_role,
+        )
+
+        step_function = _aws_stepfunctions.StateMachine(
+            self,
+            "StepFunction",
+            definition=_aws_stepfunctions_tasks.LambdaInvoke(
+                self,
+                "TrainingSubmitJob",
+                lambda_function=training_submit_lambda,
+                payload=_aws_stepfunctions.TaskInput.from_object(training_job_config),
+            ).next(_aws_stepfunctions.Succeed(self, "Success")),
+        )
+
---
+++ src/lambdas/submit_training/app.py
@@ -0,0 +1,51 @@
+import boto3
+import datetime
+import os
+
+
+def lambda_handler(event, context):
+
+    print([item for item in event.items()])
+
+    training_image = event["training_image"]
+    model_name = event["model_name"]
+    role_arn = event["role_arn"]
+    train_data_uri = event["train_data_uri"]
+    resource_config = event["resource_config"]
+    use_spot_training = event["use_spot_training"]
+
+    train_job_name = (
+        f"{model_name}-{datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}"
+    )
+
+    client = boto3.client("sagemaker")
+
+    response = client.create_training_job(
+        TrainingJobName=train_job_name,
+        AlgorithmSpecification={
+            "TrainingImage": training_image,
+            "TrainingInputMode": "File",
+        },
+        RoleArn=role_arn,
+        InputDataConfig=[
+            {
+                "ChannelName": "train",
+                "DataSource": {
+                    "S3DataSource": {
+                        "S3DataType": "S3Prefix",
+                        "S3Uri": f"{train_data_uri}",
+                    }
+                },
+            }
+        ],
+        OutputDataConfig={"S3OutputPath": f"{train_data_uri}/training_output"},
+        ResourceConfig={
+            "InstanceType": resource_config["instance_type"],
+            "InstanceCount": resource_config["instance_count"],
+            "VolumeSizeInGB": resource_config["volume_size"],
+        },
+        EnableManagedSpotTraining=use_spot_training,
+        StoppingCondition={"MaxRuntimeInSeconds": 900, "MaxWaitTimeInSeconds": 1000,},
+    )
+
+    return {"statusCode": 200}
